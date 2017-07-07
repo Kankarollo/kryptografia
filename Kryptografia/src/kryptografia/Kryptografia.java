67167abe51411f8d62f14a0c85584d0d82c62878
Leonard Sikorski <leonard.sikorski.S@ee.pw.edu.pl>
2017-07-07 20:07:41 +0200

    Pierwszy commit - pusty projekt

---
+++ Kryptografia/src/kryptografia/Kryptografia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kryptografia;
+
+/**
+ *
+ * @author Leonard
+ */
+public class Kryptografia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
