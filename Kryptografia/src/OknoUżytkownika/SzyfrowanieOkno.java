0996f472f04c92d1c6c5a842aea7f429155ff0ad
Leonard Sikorski <leonard.sikorski.S@ee.pw.edu.pl>
2017-07-07 21:26:00 +0200

    Trochę shitu

---
+++ Kryptografia/src/OknoUżytkownika/SzyfrowanieOkno.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OknoUżytkownika;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Leonard
+ */
+public class SzyfrowanieOkno extends JFrame{
+    
+    JButton bCezar, bVigenere, bHarcerskie;
+    
+    public SzyfrowanieOkno(){
+        super("Szyfrowanie");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocation(50,50);
+        setSize(300,100);
+        setLayout(new FlowLayout());
+        
+        bCezar = new JButton("Szyfr Cezara");
+        bVigenere = new JButton("Szyfr Vigenere");
+        bHarcerskie = new JButton("Szyfry Harcerskie");
+        add(bCezar);
+        add(bVigenere);
+        add(bHarcerskie);
+    }
+}
