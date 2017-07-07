e20a726aea9cd069c575b1fa4d5bf54fd60793e7
Leonard Sikorski <leonard.sikorski.S@ee.pw.edu.pl>
2017-07-07 21:25:08 +0200

    Trochę shitu

--- Kryptografia/src/OknoUżytkownika/OknoStartowe.java
+++ Kryptografia/src/OknoUżytkownika/OknoStartowe.java
@@ -5,23 +5,55 @@
  */
 package OknoUżytkownika;
 
+import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 /**
  *
  * @author Leonard
  */
-public class OknoStartowe extends JFrame{
+public class OknoStartowe extends JFrame implements ActionListener{
  
+    JButton bSzyfruj, bDeszyfruj;
+    
     public OknoStartowe() {
         super("Zarombista Aplikacja");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocation(50,50);
         setSize(300,100);
         setLayout(new FlowLayout());
-        add(new JButton("Szyfruj"));
-	add(new JButton("Deszyfruj"));
+        
+        bSzyfruj = new JButton("Szyfruj");
+        bDeszyfruj = new JButton("Deszyfruj");
+        add(bSzyfruj);
+	add(bDeszyfruj);
+        bSzyfruj.addActionListener(this);
+        bDeszyfruj.addActionListener(this);
+        
         setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if(src == bSzyfruj){
+            EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SzyfrowanieOkno();
+            }
+            });
+        }
+        else{
+            EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new OknoStartowe();
+            }
+            });
+        }
     }
 }
