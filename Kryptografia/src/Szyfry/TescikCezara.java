65ce6e944fc1546b481b0876ad9c24e79cb68a51
Kankarollo <pawel.kankowski@gmail.com>
2017-07-07 23:18:54 +0200

    Cezar

---
+++ Kryptografia/src/Szyfry/TescikCezara.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Szyfry;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Kanka
+ */
+public class TescikCezara {
+    
+    public static void main(String[] args) throws IOException
+    {
+        Cezar c = new Cezar();
+        c.Cezar("C:\\Users\\Kanka\\Desktop\\Paweł\\Programowanie+\\Cezar.txt","C:\\Users\\Kanka\\Desktop\\Paweł\\Programowanie+\\CezarOdk.txt",5);
+    }
+}
